4a573296af1a52e2863914d7c21dc8de3ecfffe2
Parvej-Hoque-Palash <cloudnumber981@gmail.com>
2023-01-18 01:45:53 +0600

    added full calculator

--- script.js
+++ script.js
@@ -8,6 +8,21 @@ function calculateGrade() {
   if (marks >= 80 && marks <= 100) {
     document.getElementById("grade").innerText = "A+";
   }
+  else if (marks >= 70 && marks <= 79) {
+    document.getElementById("grade").innerText = "A";
+  }
+  else if (marks >= 60 && marks <= 69) {
+    document.getElementById("grade").innerText = "A-";
+  }
+  else if (marks >= 50 && marks <= 59) {
+    document.getElementById("grade").innerText = "B";
+  }
+  else if (marks >= 40 && marks <= 49) {
+    document.getElementById("grade").innerText = "C";
+  }
+  else {
+    document.getElementById("grade").innerText = "F";
+  }
 }
 
 setInitialValue();
